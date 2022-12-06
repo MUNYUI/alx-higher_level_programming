a885bfa71a571ce27d05f9e470dd1d40b335dca4
Munyui <munyuinloise@gmail.com>
2022-12-06 03:18:24 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,19 +2,19 @@
 import sys
 if __name__ != "__main__":
     exit()
-       
-       argStr = "{:d} argument"
-       argc = len(sys.argv) - 1
-       if argc == 0:
-           argStr += 's.'
-       elif argc == 1:
-           argStr += ':'
-       else:
-           argStr += 's:'
-            print(argStr.format(argc))
 
-            i = 0
-            for arg in sys.argv:
-                if i != 0:
-                    print("{:d}: {:s}".format(i, arg))
-                    i += 1
+    argStr = "{:d} argument"
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        argStr += 's.'
+    elif argc == 1:
+        argStr += ':'
+    else:
+        argStr += 's:'
+        print(argStr.format(argc))
+
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{:d}: {:s}".format(i, arg))
+                i += 1
